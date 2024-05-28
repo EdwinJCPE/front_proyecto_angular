bdd819236a01d4a84f350b782e89532361d7467c
Edwin Juarez <edwinjuarez24x@gmail.com>
2024-05-27 23:06:40 -0500

    Registrar módulos, componentes y rutas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { WebModule } from './web/web.module';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', loadChildren: () => import('./web/web.module').then(w => w.WebModule) },
+  { path: 'auth', loadChildren: () => import('./auth/auth.module').then(a => a.AuthModule ) },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(ad => ad.AdminModule ) },
+];
